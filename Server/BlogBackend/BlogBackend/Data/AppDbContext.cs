d90f94a9096e1999961afb925d116b59ea43b67a
Ritchie512 <richarescobedo512@gmail.com>
2025-07-17 23:54:12 -0400

    Avance: Configuración inicial de DBContext y migraciones

---
+++ Server/BlogBackend/BlogBackend/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BlogBackend.Models;
+
+namespace BlogBackend.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    // Tablas de la base de datos
+    public DbSet<Post> Posts { get; set; }
+    // Añade más DbSets aquí si necesitas (ejem: DbSet<Comment> Comments)
+}
---
+++ Server/BlogBackend/BlogBackend/Models/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogBackend.Models
+{
+    public class Post
+    {
+        public int Id { get; set; } //Clave primaria
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
